daa79ceffe3f68a349ae82979c712e78f06cb36d
7xstack <chaiyanan.ch2006@gmail.com>
2026-09-22 20:45:27 +0700

    Update advanced_mult.py

--- discovery_piscine/cell03/ex03/advanced_mult.py
+++ discovery_piscine/cell03/ex03/advanced_mult.py
@@ -1,12 +1,18 @@
 import sys
-if len(sys.argv) > 1:
+def main():
+    if len(sys.argv) > 1:
         print("none")
-i = 0
-while i <= 10:
-    print(f"Table de {i}:", end="")
-    j = 0
-    while j <= 10:
-        print(f" {i * j}", end="")
-        j += 1
-    print()
-    i += 1
+        return
+
+    i = 0
+    while i <= 10:
+        print(f"Table de {i}:", end="")
+        j = 0
+        while j <= 10:
+            print(f" {i * j}", end="")
+            j += 1
+        print()
+        i += 1
+
+if __name__ == "__main__":
+    main()
